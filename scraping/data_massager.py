9bd3666971c89ecdd86fb1ab06142aad9f8d4b3a
jeffreyfluhrer <jeffreyfluhrer@hotmail.com>
2018-03-28 22:39:41 +1200

    data_massager will produce all required tables -- a bit primative

--- scraping/data_massager.py
+++ scraping/data_massager.py
@@ -1,16 +1,19 @@
 import csv
 import re
+import random
 
 # This script will produce three files that will be the csv inputs for the Resort, StayPricing and
 # WeatherForecast tables.
 # Resort schema is Resort(ResortName, City, State, SeasonStartDAte, SeasonEndDate, ImageuRL, rating, difficulty)
-# 
+# StayPricing schema is StayPricing (Resortname, Date, StayPrice, LiftTicketPrice, BookingURL)
+# WeatherForceast schema is WeatherForecast (ResortName, Date, SnowDepth, Status)
 
 def massage(file1):
     reader1 = csv.reader(open(file1, 'r'))
     resortCSV = csv.writer(open('resort.out','w'))
     stayCSV = csv.writer(open('staypricing.out','w'))
     weatherCSV = csv.writer(open('weatherforecast.out','w'))
+    flightCSV = csv.writer(open('flight.out','w'))    
 
     # Define the lists
     Resort = []
@@ -39,21 +42,74 @@ def massage(file1):
         snowDepth.append(row[9])
         status.append(row[10])
 
+    # Massage data for Resort table
     Resort = manipResort(Resort)
     ticketPrice = manipPrice(ticketPrice)
     difficulty = manipDifficulty(beginner, intermed, advanced)
     startSeason = manipStartSeason(season)
     endSeason = manipEndSeason(season)
     rating = manipRating(rating)
-    writeResort(resortCSV, Resort, ticketPrice, difficulty, startSeason, endSeason, image, rating)
+    location = createLocations(Resort)
+
+    # Output data for the Resort table
+    writeResort(resortCSV, Resort, ticketPrice, difficulty, startSeason, endSeason, image, rating, location)
+
+    # Massage the weather forecast data
+    weatherDate = manipUpdateDate(updateDate)
+    snowDepth = manipSnowDepth(snowDepth)
+    status = manipStatus(status)
+
+    # Output data for the WeatherForecast table
+    writeWeather(weatherCSV, Resort, weatherDate, snowDepth, status)
+
+    # Massage the StayPricing data
+    #date = createDates();
+    date = '2018-03-23'
+    multiplier = 1.0;
+    stayPrice = createStayPrice(rating, multiplier)
+    bookingURL = createBooking(Resort)
+
+    # Output data for the StayPricing table
+    writePricing(stayCSV, Resort, date, stayPrice, ticketPrice, bookingURL)
+
+    # Massage the Flight table data
+    startCities = ['Chicago', 'New York', 'Washington D.C.', 'Boston', 'Los Angeles', 'Seattle']
+    date = '2018-03-23'
+    for city in startCities:
+        trip = createFlight(Resort)
+        writeFlight(flightCSV, city, Resort, date, trip)
                                                                              
-def writeResort(csv, resort, price, difficulty, startSeason, endSeason, image, rating):
+def writeResort(csv, resort, price, difficulty, startSeason, endSeason, image, rating, location):
     length = len(resort)
     i = 0
     while i < length:
-        fields = (resort[i],"city", "state", startSeason[i], endSeason[i], image[i], rating[i], difficulty[i])
+        fields = (resort[i],location[i][0], location[i][1], startSeason[i], endSeason[i], image[i], rating[i], difficulty[i])
         csv.writerow(fields)
         i = i + 1            
+
+def writeWeather(csv, resort, updateDate, snowDepth, status):
+    length = len(resort)
+    i = 0
+    while i < length:
+        fields = (resort[i],updateDate[i], snowDepth[i], status[i])
+        csv.writerow(fields)
+        i = i + 1  
+
+def writePricing(csv, resort, date, stayPrice, ticketPrice, bookingURL):
+    length = len(resort)
+    i = 0
+    while i < length:
+        fields = (resort[i], date, stayPrice[i], ticketPrice[i], bookingURL[i])
+        csv.writerow(fields)
+        i = i + 1 
+
+def writeFlight(csv, city, resort, date, trip):
+    length = len(resort)
+    i = 0
+    while i < length:
+        fields = (city, resort[i], date, trip[i][0], trip[i][1])
+        csv.writerow(fields)
+        i = i + 1         
     
 def manipResort(data):
     newName = []
@@ -66,7 +122,7 @@ def manipPrice(data):
     for price in data:
         output = re.findall(r'\d+',price)
         if not output:
-            output = "NULL"
+            output = "100.0"
         newPrice.append(output[0])
     return newPrice
     
@@ -107,7 +163,6 @@ def manipEndSeason(season):
     while i < length:
         if season[i] == "NULL":
             endDate = '2017-11-15'
-#        endDate = '2018-04-18'
         else:
             match = re.search('\d{4}-\d{2}-\d{2}', season[i])
             endDate = match.group(0)
@@ -128,6 +183,103 @@ def manipRating(rating):
         ratingOut.append(rate)
         i = i + 1
     return ratingOut
+
+def manipUpdateDate(updateDate):
+    dateOut = []
+    i = 0
+    length = len(updateDate)
+    while i < length:
+        if updateDate[i] == "NULL":
+            out = '2018-03-24'
+        else:
+            out = updateDate[i]
+        i = i + 1
+        dateOut.append(out)
+    return dateOut
+
+def manipSnowDepth(snowDepth):
+    depthOut = []
+    i = 0
+    length = len(snowDepth)
+    while i < length:
+        if snowDepth[i] == "NULL":
+            out = '30'
+        else:
+            out = re.findall(r'\d+',snowDepth[i])
+            if not out:
+                out = 30
+            else:
+                out = out[0]
+        i = i + 1
+        depthOut.append(out)
+    return depthOut
+
+def manipStatus(status):
+    statusOut = []
+    i = 0
+    length = len(status)
+    while i < length:        
+        statusVal = status[i].lower()
+        statusVal = re.findall(r'open',statusVal)
+        if not statusVal:
+            statusOut.append("Closed")
+        elif status[i] == 'NULL':
+            statusOut.append("Closed")
+        else:
+            statusOut.append("Open")
+        i = i + 1
+    return statusOut
+
+# This routine will brew up the cost to stay in this location
+def createStayPrice(rating, multiplier):
+    stayPrice = []
+    i = 0
+    length = len(rating)
+    while i < length:
+        cost = 45.0 * float(rating[i]) * multiplier + random.randint(1,100)
+        stayPrice.append(str(cost))
+        i = i + 1
+    return stayPrice
+
+#def createDates():
+def createBooking(Resort):
+    booking = []
+    i = 0
+    length = len(Resort)
+    while i < length:
+        booking.append('www.expedia.com')
+        i = i + 1
+    return booking
+
+def createLocations(Resort):
+    location = []
+    i = 0
+    length = len(Resort)
+    while i < length:
+        location.append(getLocation())
+        i = i + 1
+    return location
+         
+def getLocation():
+    Locations = [['Salt Lake City', 'Utah'], ['Park City', 'Utah'], ['Camden', 'Maine'], ['Jackson', 'New Hampshire'], ['Bolton', 'Vermont'],
+                 ['Vernon', 'New Jersey'], ['Union Dale', 'Pennsylvania'], ['Boyne City', 'Michigan'], ['Burnsville', 'Minnesota'],
+                 ['Hansen', 'Idaho']]
+    length = len(Locations) - 1
+    return Locations[random.randint(1,length)]
+    
+
+def createFlight(resort):
+    Airlines = ["United", "American", "Delta", "Jet Blue", "SouthWest"]
+    airLen = len(Airlines) - 1
+    trip = []
+    i = 0
+    length = len(resort)
+    while i < length:
+        price = random.randint(251,704)
+        airline = Airlines[random.randint(1,airLen)]
+        trip.append([price, airline])
+        i = i + 1
+    return trip
 
 def Entry2Float(str):
     paren = re.search('\(([^)]+)', str).group(1)
